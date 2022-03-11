cde66e55b58e0526e3b8b7d7d79a6e1cb1662c41
philipe_kraken <philipeacampos@hotmail.com>
2022-03-11 17:11:49 -0300

    testes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { Header } from './components/Header';
+import { Attendance } from './components/Attendance';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Attendance />
     </div>
   );
 }
--- src/components/Attendance/index.js
+++ src/components/Attendance/index.js
@@ -1,5 +1,21 @@
+import styles from './styles.module.css';
+
 export function Attendance() {
     return(
-        <h1>Attendance</h1>
+        <main className={styles.container}>
+            <section className={styles.content}>
+                <div>
+                    <header>
+                        <p>hdjahdhsdj jh dj sjd j dj dja dhs dsajhdjk dkj hdjk dkj hdjkahd</p>
+                    </header>
+                    
+                    <button>
+                        Fale Conosco
+                    </button>
+                </div>
+
+            </section>
+
+        </main>
     )
 }
