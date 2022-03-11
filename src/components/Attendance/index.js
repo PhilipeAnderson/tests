0b5e2d7d9be01c028aabc9e705552ce1de70e447
philipe_kraken <philipeacampos@hotmail.com>
2022-03-11 16:05:02 -0300

    testes

---
+++ src/components/Attendance/index.js
@@ -0,0 +1,5 @@
+export function Attendance() {
+    return(
+        <h1>Attendance</h1>
+    )
+}
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,23 @@
+import styles from './styles.module.css';
+
 export function Header() {
   return (
-    <h1>Olá</h1>
+    <header className={styles.container}>
+      <main className={styles.content}>
+        <nav>
+          <ul className={styles.menu}>
+            <li><a className={styles.textMenu} href="/">Início</a></li>
+            <li><a className={styles.textMenu} href="/">Sobre o Reiki</a></li>
+          </ul>
+        </nav>
+        <div>
+          <ul className={styles.socialNet}>
+            <li className={styles.icons}>Insta</li>
+            <li className={styles.icons}>Face</li>
+            <li className={styles.icons}>Whats</li>
+          </ul>
+        </div>
+      </main>
+    </header>
   )
 }
