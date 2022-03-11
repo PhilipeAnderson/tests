b75cc0a0b8e639734132bb73135f174d5219c176
philipe_kraken <philipeacampos@hotmail.com>
2022-03-11 17:01:20 -0300

    testes

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import styles from './styles.module.css';
+import Lotus from '../../assets/lotus.png';
 import { FaFacebook, FaInstagram, FaWhatsapp } from 'react-icons/fa';
 
 export function Header() {
@@ -31,6 +32,13 @@ export function Header() {
           </ul>
         </div>
       </main>
+      <section className={styles.bodyHeader}>
+        <h1 className={styles.textBody}>
+        O Reiki é uma ferramenta natural que pode ser usada 
+        para nos ajudar a lidar com as tensões da vida cotidiana.
+        </h1>
+        <img className={styles.lotus} src={Lotus} alt="Flor de Lotus" />
+      </section>
     </header>
   )
 }
