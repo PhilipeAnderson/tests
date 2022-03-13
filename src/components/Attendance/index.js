c9ea5395f1d2bc86f7f7249f4e2c1411f48f1a31
philipe_kraken <philipeacampos@hotmail.com>
2022-03-12 23:31:17 -0300

    testes

--- src/components/Attendance/index.js
+++ src/components/Attendance/index.js
@@ -4,8 +4,8 @@ export function Attendance() {
     return(
         <main className={styles.container}>
             <section className={styles.content}>
-                <div>
-                    <header>
+                <div className={styles.boxAttendance}>
+                    <header className={styles.headerBox}>
                         <p>hdjahdhsdj jh dj sjd j dj dja dhs dsajhdjk dkj hdjk dkj hdjkahd</p>
                     </header>
                     
@@ -13,7 +13,24 @@ export function Attendance() {
                         Fale Conosco
                     </button>
                 </div>
-
+                <div className={styles.boxAttendance}>
+                    <header className={styles.headerBox}>
+                        <p>hdjahdhsdj jh dj sjd j dj dja dhs dsajhdjk dkj hdjk dkj hdjkahd</p>
+                    </header>
+                    
+                    <button>
+                        Fale Conosco
+                    </button>
+                </div>
+                <div className={styles.boxAttendance}>
+                    <header className={styles.headerBox}>
+                        <p>hdjahdhsdj jh dj sjd j dj dja dhs dsajhdjk dkj hdjk dkj hdjkahd</p>
+                    </header>
+                    
+                    <button>
+                        Fale Conosco
+                    </button>
+                </div>
             </section>
 
         </main>
