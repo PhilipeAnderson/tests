8897e26ea6658b2d10ae8c2f6335af5119588ff4
philipe_kraken <philipeacampos@hotmail.com>
2022-03-21 20:27:32 -0300

    tests

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { Header } from './components/Header';
 import { Attendance } from './components/Attendance';
+import { Testimonial } from './components/Testimonial';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Attendance />
+      <Testimonial />
     </div>
   );
 }
