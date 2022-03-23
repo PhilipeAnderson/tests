d85c1108d112b6489425052afdb75e21b41a2b87
philipe_kraken <philipeacampos@hotmail.com>
2022-03-23 20:50:22 -0300

    tests

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Header } from './components/Header';
 import { Attendance } from './components/Attendance';
 import { Testimonial } from './components/Testimonial';
 import { About } from './components/About';
+import { Footer } from './components/Footer';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Attendance />
       <Testimonial />
       <About />
+      <Footer />
     </div>
   );
 }
