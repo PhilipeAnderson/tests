16f9e9695bd0115d63e41adad43af1d561710ad0
philipe_kraken <philipeacampos@hotmail.com>
2022-03-23 20:02:37 -0300

    tests

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,9 +1,26 @@
 import styles from './styles.module.css';
+import Reiki from '../../assets/jpr.png';
+
 
 export function About() {
   return(
-    <main>
-      <h1 className={styles.container}>Teste</h1>
+    <main className={styles.container}>
+      <section className={styles.content}>
+        <h1 className={styles.textAbout}>Reiki</h1>
+
+        <aside className={styles.left}>
+
+        </aside>
+
+        <section className={styles.center}>
+          <img src={Reiki} alt="Letras Japonesas falando sobre o Reiki" />
+        </section>
+
+        <aside className={styles.right}>
+          
+        </aside>
+
+      </section>
     </main>
 
   )
