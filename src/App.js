336e2b66d7936327689ea519e4cf0137b9be813b
philipe_kraken <philipeacampos@hotmail.com>
2022-03-24 15:53:43 -0300

    tests

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
-import './App.css';
+import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+
 import { Home } from './pages/index';
 import { Reiki } from './pages/Reiki';
+
+import './App.css';
 
 function App() {
   return (
