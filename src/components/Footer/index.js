0739cba9252ffca3421670d76b5230ac4e614673
philipe_kraken <philipeacampos@hotmail.com>
2022-03-23 21:00:42 -0300

    tests

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,20 @@
 import styles from './styles.module.css';
+import { FaFacebook, FaInstagram, FaWhatsapp } from 'react-icons/fa';
 
 export function Footer() {
   return(
-    <main className={styles.container}>Teste Footer</main>
+    <main className={styles.container}>
+      <section className={styles.content}>
+        <article className={styles.netSocial}>
+          <h1>Siga-nos nas redes sociais</h1>
+          <p>Faça parte da nossa Comunidade !</p>
+          <footer>
+            <i><FaWhatsapp /></i>
+            <i><FaFacebook /></i>
+            <i><FaInstagram /></i>
+          </footer>
+        </article>
+      </section>
+    </main>
   )
 }
