d150fd253928cd3f051d1ce91102ae79d00e6315
philipe_kraken <philipeacampos@hotmail.com>
2022-03-25 13:22:55 -0300

    tests

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
 import { Home } from './pages/index';
 import { Reiki } from './pages/Reiki';
@@ -9,8 +9,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Home />
-      <Reiki />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/Reiki' element={<Reiki />} />
+      </Routes>
     </div>
   );
 }
