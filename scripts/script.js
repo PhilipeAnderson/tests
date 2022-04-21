139b712fc1e23fbc2dc2a4bff0c6c7b93a806517
philipe_kraken <philipeacampos@hotmail.com>
2022-04-21 18:09:10 -0300

    Deleted all project

--- scripts/script.js
+++
@@ -1,7 +0,0 @@
-const input = document.querySelector('#input')
-const email = document.querySelector('#email')
-
-
-const mostraDados = () => {
-    alert(`Seu nome é: ${input.value} e seu email é ${email.value}`)
-}
