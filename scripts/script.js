b3cd9179e0290fb2fd08f46ca5f8e1e1d290290a
philipe_kraken <philipeacampos@hotmail.com>
2022-04-21 18:05:31 -0300

    fixed

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
-const input = document.querySelector('#input').value;
-const email = document.querySelector('#email').value;
+const input = document.querySelector('#input')
+const email = document.querySelector('#email')
 
 
 const mostraDados = () => {
-    alert(`Seu nome é: ${input} e seu email é ${email}`)
+    alert(`Seu nome é: ${input.value} e seu email é ${email.value}`)
 }
