a8a8844c1114ea8b5e6f035130acf434d96a5f28
philipe_kraken <philipeacampos@hotmail.com>
2022-04-21 18:48:43 -0300

    back the last version

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+const input = document.querySelector('#input')
+const email = document.querySelector('#email')
+
+
+const mostraDados = () => {
+    alert(`Seu nome é: ${input.value} e seu email é ${email.value}`)
+}
